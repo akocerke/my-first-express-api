6dd3154d5a983582e81476c3242b0e3c8424cfb2
akocerke <152269613+akocerke@users.noreply.github.com>
2024-02-15 14:05:40 +0100

    update

--- frontend/src/Pages/Todo/Todo.js
+++ frontend/src/Pages/Todo/Todo.js
@@ -6,28 +6,11 @@ const Todo = () => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:5050/v1/todos/byuserid", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ userId: 1 })
-        });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        
-        const data = await response.json();
-        setTodos(data); // Setze die abgerufenen Todos im State
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    fetchTodos();
-  }, []); // Leere Abhängigkeitsliste bedeutet, dass diese Effektfunktion nur einmal ausgeführt wird
+    fetch("http://localhost:3030/v1/todos/all")
+      .then(response => response.json())
+      .then(data => setTodos(data.todos))
+      .catch(error => console.error('Fehler beim Abrufen der Todos:', error));
+  }, []);
 
   return (
     <Content>
@@ -35,10 +18,7 @@ const Todo = () => {
         <h2>Meine ToDos</h2>
         <ul>
           {todos.map(todo => (
-            <li key={todo.id}>
-              <input type="checkbox" checked={todo.completed} />
-              <span className={styles.text}>{todo.title}</span>
-            </li>
+            <li key={todo.id}>{todo.title}</li>
           ))}
         </ul>
       </div>
